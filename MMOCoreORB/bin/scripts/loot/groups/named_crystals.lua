fde2b1e91dab9bbfe943aa4c518b5693270b47d6
Author Name <5497734+randyreynolds@users.noreply.github.com>
2020-06-18 20:20:36 -0700

    updated with named color crystals

---
+++ MMOCoreORB/bin/scripts/loot/groups/named_crystals.lua
@@ -0,0 +1,28 @@
+named_crystals = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "crystal_sunriders_destiny", weight = 10000000},
+		{itemTemplate = "crystal_mauls_vengence", weight = 10000000},
+		{itemTemplate = "crystal_kuns_blood", weight = 10000000},
+		{itemTemplate = "crystal_bondaras_folly", weight = 10000000},
+		{itemTemplate = "crystal_ulics_redemption", weight = 10000000},
+		{itemTemplate = "crystal_banes_heart", weight = 10000000},
+		{itemTemplate = "crystal_gallias_intuition", weight = 10000000},
+		{itemTemplate = "crystal_prowess_of_plo_koon", weight = 10000000},
+		{itemTemplate = "crystal_bnars_sacrifice", weight = 10000000},
+		{itemTemplate = "crystal_windus_guile", weight = 10000000},
+		{itemTemplate = "crystal_mundis_response", weight = 10000000},
+		{itemTemplate = "crystal_strength_of_luminara", weight = 10000000},
+		{itemTemplate = "crystal_baass_wisdom", weight = 10000000},
+		{itemTemplate = "crystal_quintessence_of_the_force", weight = 10000000},
+		{itemTemplate = "crystal_dawn_of_dagobah", weight = 10000000},
+		{itemTemplate = "crystal_horns_future", weight = 10000000},
+		{itemTemplate = "crystal_quigons_devotion", weight = 10000000},
+		{itemTemplate = "crystal_kenobis_legacy", weight = 10000000},
+		{itemTemplate = "crystal_kits_ferocity", weight = 10000000},
+	}
+}
+
+addLootGroupTemplate("named_crystals", named_crystals)
